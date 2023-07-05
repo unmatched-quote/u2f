1f4e6dea2d93dea6879fc5fa564d1c7c58cc93ab
Nikola B <nikola.bursac@proton.me>
2023-07-05 09:20:17 +0000

    Add #[SensitiveParameter] to DTO to avoid saving private key to log files in case of errors

--- src/DTO/DecodedRegistrationResponse.php
+++ src/DTO/DecodedRegistrationResponse.php
@@ -2,12 +2,25 @@
 
 namespace JustSomeCode\U2F\DTO;
 
+use Attribute;
+use function JustSomeCode\U2F\u2f_str_encode;
+
 final readonly class DecodedRegistrationResponse
 {
     public function __construct(
         public string $pubKey,
         public string $keyHandle,
-        public string $certificate,
+        #[\SensitiveParameter(Attribute::TARGET_PROPERTY)] public string $certificate,
         public string $appId
     ){}
+
+    public function __serialize(): array
+    {
+        return [
+            base64_encode($this->pubKey),
+            u2f_str_encode($this->keyHandle),
+            base64_encode($this->certificate),
+            $this->appId
+        ];
+    }
 }
