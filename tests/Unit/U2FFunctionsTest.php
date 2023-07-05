3c25f103c5921c15235138c82a6ff2d4ce0afb22
Nikola B <nikola.bursac@proton.me>
2023-07-05 09:51:57 +0000

    Add u2f_auth_challenge test

--- tests/Unit/U2FFunctionsTest.php
+++ tests/Unit/U2FFunctionsTest.php
@@ -3,8 +3,10 @@
 namespace JustSomeCode\U2F\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use JustSomeCode\U2F\DTO\AuthenticationChallenge;
 use JustSomeCode\U2F\DTO\DecodedRegistrationResponse;
 use function JustSomeCode\U2F\u2f_enroll_parse;
+use function JustSomeCode\U2F\u2f_auth_challenge;
 use function JustSomeCode\U2F\u2f_enroll_challenge;
 
 class U2FFunctionsTest extends TestCase
@@ -34,6 +36,18 @@ class U2FFunctionsTest extends TestCase
         $this->assertInstanceOf(DecodedRegistrationResponse::class, $result);
     }
 
+    public function test_u2f_auth_challenge_function(): void
+    {
+        $result = u2f_auth_challenge(...$this->provideValidU2FKeyData());
+
+        $this->assertInstanceOf(AuthenticationChallenge::class, $result);
+        $this->assertIsArray($data = (array)$result);
+        $this->assertArrayHasKey('appId', $data['appId']);
+        $this->assertArrayHasKey('challenge', $data['challenge']);
+        $this->assertArrayHasKey('keyHandle', $data['keyHandle']);
+        $this->assertArrayHasKey('version', $data['version']);
+    }
+
     protected function provideValidAppId(): array
     {
         return [
@@ -47,4 +61,12 @@ class U2FFunctionsTest extends TestCase
         return require(__DIR__ . '/../Data/valid_registration_challenge_response_data.php');
     }
 
+    protected function provideValidU2FKeyData(): array
+    {
+        $data = require(__DIR__ . '/../Data/valid_u2f_key.php');
+
+        if(isset($data['version'])) unset($data['version']);
+
+        return $data;
+    }
 }
